148648b57405e08138d97eb637fbfdef94eb2779
katriellucas <katriel.developer@gmail.com>
2022-12-08 21:07:05 -0300

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      edge: false,
+      split: false
+    })
 	}
 };
 
